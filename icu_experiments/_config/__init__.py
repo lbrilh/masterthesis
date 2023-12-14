134076d2e7412ac5f81857598a216907dff2ef38
lbrilh <lbrilhaus@gmail.com>
2023-12-14 15:20:49 +0100

    avoind accessing maltes account on cluster

--- icu_experiments/_config/__init__.py
+++ icu_experiments/_config/__init__.py
@@ -4,7 +4,7 @@ from .load_config import load_config
 
 config = {}
 
-for p in Path(__file__).parent.glob("*.json"):
+'''for p in Path(__file__).parent.glob("*.json"):
     config_ = load_config(p)
     if Path(config_["mlflow"]["tracking_uri"].replace("file:", "")).exists():
-        config = config_
+        config = config_'''
