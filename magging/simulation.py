e4cdbca581689b4307b6460527e06c0d0a7a8ac8
lbrilh <lbrilhaus@gmail.com>
2024-01-31 20:05:12 +0100

    Simulations.py adjusted matrix

--- magging/simulation.py
+++ magging/simulation.py
@@ -1,3 +1,10 @@
+''' This script calculates the magging estimator for an artifical dataset and plots it (2D).
+
+ToDo: Calculate magging distance for this data. Verify results by hand.
+'''
+
+
+
 from magging import Magging
 import numpy as np
 from cvxopt import matrix, solvers
@@ -5,9 +12,9 @@ import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
 
 
-X = np.identity(3)
+X = np.identity(3) # predictor matrix 
 
-B = np.matrix('1 1 2; 1 -1 1')
+B = np.matrix('1 1 2; 1 -1 1') # matrix containing group coefficient estimators
 
 fhat = np.matrix('1 1 2; 1 -1 1') # fhat == B 
 
@@ -32,41 +39,31 @@ b = matrix(1.0)
 # Solve the quadratic program to obtain magging weights
 solution = solvers.qp(P, q, G, h, A, b)
 w = np.array(solution['x']).round(4).flatten() # Magging weights
-
+print(w)
+print()
 # Your matrix B
-B = np.matrix('1 1 2; 1 -1 1')
-
+B = np.matrix('0.5 1.5 2; 1 0.5 1')
+print(B)
+print(B[0:2,0:2])
 # Create a figure and axis for the plot
 fig, ax = plt.subplots()
 
-# Plot the column vectors of B as points
-ax.plot(B[0], B[1], 'o', markersize=8)
-
-# Connect the points of B with lines to form the convex hull
-ax.plot(*B[:, [0, 1, 2, 0]], 'k-')  # Connecting points in sequence and back to the start
-
-# Plot the red line (b_maximin line) along the x-axis
-ax.plot([0, 1], [0, 0], 'r-')
-
-# Find the point with the largest x-coordinate, which is on or below the red line (y <= 0)
-# This point is the intersection with the convex hull
-b_maximin_point = B.T[np.argmin(B[1, :])]
-b_maximin_x, b_maximin_y = b_maximin_point[0, 0], b_maximin_point[0, 1]
-
-# Plot the point b_maximin
-ax.plot(b_maximin_x, b_maximin_y, 'ro')  # Red point
-
-# Annotate the point b_maximin
-ax.annotate('b_maximin', (b_maximin_x, b_maximin_y), textcoords="offset points", xytext=(5, -5))
-
-# Set labels for the axes
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-
-# Show the plot
+ax.plot([B[0,0],B[0,1]], [B[1,0],B[1,1]], '-ok')
+ax.annotate('b_1', xy=(.5,1.05))
+ax.plot([B[0,0],B[0,2]], [B[1,0],B[1,2]], '-ok')
+ax.annotate('b_2', xy=(1.55,0.5))
+ax.plot([B[0,1],B[0,2]], [B[1,1],B[1,2]], '-ok')
+ax.annotate('b_3', xy=(2,1.05))
+ax.plot([0,1], [0,0.75], 'r-')
+ax.plot(1.,0.75, 'or')
+ax.annotate('b_maximin', xy=(1.05,0.75), color='red')
+ax.set(xlim=(-0,2.5), ylim=(0,1.5))
+ax.spines[['top','right']].set_visible(False)
+ax.plot(0.5,0.5, 'ob')
+ax.annotate('b_DSL', xy=(0.5,0.55), color='blue')
+plt.xticks([0,1,2])
+plt.yticks([0,0.5,1,1.5])
 plt.show()
-
-print(w)
 raise ValueError
 if isinstance(self.model, (Lasso, LinearRegression)):
     coef = []
