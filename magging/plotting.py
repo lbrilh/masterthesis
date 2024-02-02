b545a5152d96d3b860c6541db2e5143b20e637ef
lbrilh <lbrilhaus@gmail.com>
2024-02-02 23:28:58 +0100

    ToDo's plotting.py

--- magging/plotting.py
+++ magging/plotting.py
@@ -1,8 +1,5 @@
 '''This file creates a figure containing a boxplot and a kernel-density estimator plot for 
 the average hr 48-72h after admission categorized by age group. 
-
-ToDo:   1. Drop categories with 0 entries.  
-        2. Add number of observations in plots    
 '''
 
 
@@ -46,11 +43,29 @@ else:
     for dataset in ['eicu', 'hirid', 'miiv', 'mimic']:
         _Xydata[dataset][category].fillna(value='N/A', inplace=True)
 
+# Population pie charts
+for dataset in ['eicu', 'hirid', 'mimic', 'miiv']:
+    _Xydata[dataset][category] = _Xydata[dataset][category].cat.remove_unused_categories()
+    _dfcategories = _Xydata[dataset][category].cat.categories
+    len_dataset = _Xydata[dataset][category].shape[0]
+    labels = _Xydata[dataset][category].value_counts().sort_index()
+    fig, ax = plt.subplots()
+    print(sns.color_palette('crest'))
+    if dataset == 'eicu': 
+        colors = [(0.48942421, 0.72854938, 0.56751036), (0.24929311, 0.56486397, 0.5586654), (0.11131735, 0.39155635, 0.53422678), (0.14573579, 0.29354139, 0.49847009)]
+    elif dataset in ['hirid', 'miiv']: 
+        colors = [(0.24929311, 0.56486397, 0.5586654),(0.15615526, 0.47913212, 0.54850526),(0.14573579, 0.29354139, 0.49847009)]
+    elif dataset == 'mimic':
+        colors = [(0.34892097, 0.64828676, 0.56513633), (0.24929311, 0.56486397, 0.5586654), (0.11131735, 0.39155635, 0.53422678), (0.14573579, 0.29354139, 0.49847009)]
+    ax.pie(x=_Xydata[dataset][category].value_counts().sort_index(), labels=_dfcategories, colors=colors, 
+           wedgeprops={"linewidth": 1, "edgecolor": "white", "alpha": 0.6},
+           autopct=lambda p: int(p/100*len_dataset))
+    plt.title(f'Population in {dataset}', fontdict={'fontweight': 'bold', 'fontsize': 15} )
 
 for dataset in ['eicu', 'hirid', 'miiv', 'mimic']:
     fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10,5))
     df_categories = _Xydata[dataset][category].cat.categories # ordered categories from DataFrame
-    print(_Xydata[dataset][category].value_counts())
+    _Xydata[dataset][category]=_Xydata[dataset][category].cat.remove_unused_categories()
 
     # Boxplot
     plt.subplot(1,2,1)
@@ -75,4 +90,5 @@ for dataset in ['eicu', 'hirid', 'miiv', 'mimic']:
     fig.supxlabel('average hr 48-72h')
     plt.setp(ax.spines.values(),linewidth=1)
     plt.setp(f.spines.values(),linewidth=1)
+
 plt.show()
