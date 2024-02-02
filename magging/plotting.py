4046b924757b5578c24f271f9b7fd44a44726f7f
lbrilh <lbrilhaus@gmail.com>
2024-02-02 17:01:50 +0100

    Generalized Plotting.py

--- magging/plotting.py
+++ magging/plotting.py
@@ -5,16 +5,20 @@ ToDo: Generalize this and allow different cateogries.
 '''
 
 
-
 import seaborn as sns 
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
+fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10,5))
+dataset = 'hirid'
 category = 'age_group'
 y = 'outcome'
-age_group = True
-dataset = 'hirid'
+
+if category == 'age_group':
+    age_group = True
+else: 
+    age_group = False
 
 # Load data from parent parquet folder 
 def load_data(outcome, source, version='train'):
@@ -34,41 +38,38 @@ _Xydata={
 
 if age_group:
     # Add category age_group to datasets
+    bins = [0, 15, 39, 65, float('inf')]
+    labels = ['child', 'young adults', 'middle age', 'senior']
     for dataset in ['eicu', 'hirid', 'miiv', 'mimic']:
-        bins = [0, 15, 39, 65, float('inf')]
-        labels = ['child', 'young adults', 'middle age', 'senior']
+        print(_Xydata[dataset]['age'])
         _Xydata[dataset]['age_group'] = pd.cut(_Xydata[dataset]['age'], bins=bins, labels=labels, right=False)
 else:
     # Fill missing values in category column
     _Xydata[dataset][category].fillna(value='N/A', inplace=True)
 
 
-print(_Xydata[dataset]['age_group'].isna().sum())
+df_categories = _Xydata[dataset][category].cat.categories # ordered categories from DataFrame
 
 
-fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10,5))
-
+# Boxplot
 plt.subplot(1,2,1)
 plt.xticks([0, 50, 100, 150])
-f = sns.boxplot(data=_Xydata[dataset], x=y, hue=category, y=category, hue_order=['child', 'young adults', 'middle age', 'senior'],
+f = sns.boxplot(data=_Xydata[dataset], x=y, hue=category, y=category, hue_order=df_categories,
                 palette='crest', legend=False, fill=True, linewidth=1, boxprops=dict(alpha=0.6))
 f.set_xlabel('',)
 f.set_ylabel('',)
 sns.set_style('ticks')
 plt.subplot(1,2,2)
-#fig.add_subplot(1,2,2, frameon=False)
 
-ax = sns.kdeplot(data=_Xydata[dataset], x='outcome', hue=category, common_norm=False, hue_order=['child', 'young adults', 'middle age', 'senior'],
+# KDE Plot
+ax = sns.kdeplot(data=_Xydata[dataset], x='outcome', hue=category, common_norm=False, hue_order=df_categories,
             palette='crest', fill=True, linewidth=0, alpha=0.5, legend=False)
-
 ax.set_xlabel('')
 ax.set_ylabel('')
-
 sns.set_style('ticks')
 sns.despine()
-#ax.set_title(f'Density of average hr 48-72h after admission categorized by age in {dataset}', fontdict={'fontweight': 'bold'})
 
-#sns.move_legend(ax, 'upper left', title=None)
+# Figure
 plt.suptitle(f'Average hr 48-72h after admission categorized by age in {dataset}', fontweight= 'bold', fontsize=15)
 fig.supxlabel('average hr 48-72h')
 plt.setp(ax.spines.values(),linewidth=1)
