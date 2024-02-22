ec1b28c9f260b53a790a7c2d0f243a96de2404d2
lbrilh <lbrilhaus@gmail.com>
2024-02-22 12:30:28 +0100

    Include Lasso Coef Profiles

---
+++ Feature Selection/profiles.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Lasso
+
+'''
+
+beta_ols = ...
+
+l1_ols = calc. l1 norm of beta_ols
+
+shrinkage_factors = np.linspace(0, l1_ols, 10)
+
+the shrinkage factor is our 
+
+'''
