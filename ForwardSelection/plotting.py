b64e2f7dabfd6a0a6c1277291369ee929aa6f937
lbrilh <lbrilhaus@gmail.com>
2024-03-30 17:18:47 +0100

    Changes

--- ForwardSelection/plotting.py
+++ ForwardSelection/plotting.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from matplotlib.cm import ScalarMappable
 from itertools import combinations
 
-outcome = 'map'
+outcome = 'hr'
 datasets = ['eicu', 'mimic', 'miiv', 'hirid']
 
 # This block generates plots for the Lasso baseline model's performance metrics, such as train and test scores, 
@@ -31,27 +31,29 @@ for r in range(2,4):
             row, col = divmod(i,2)
         ax = axs[row,col]
         data = pd.read_parquet(f'baseline_results/lasso/{outcome}/{outcome}_lasso_train_on_{group_combination}_forward_selection_results.parquet')
+        colors = {'eicu': 'blue', 'hirid': 'cyan', 'miiv': 'magenta', 'mimic': 'red', 'train': 'black'}
         for col in data.columns:
             splitted_col = col.split(' ')
             if 'name' not in col:
-                if 'train' in col:
-                    ax.plot(range(0,51), data[col], 'ko-', label=f'train, alpha={round(alpha,4)}', ms=2)
-                if 'test' in col:
-                    if 'eicu' in col: 
-                        ax.plot(range(0,51), data[col], 'bo-', label=splitted_col[2], ms=2)
-                    if 'mimic' in col: 
-                        ax.plot(range(0,51), data[col], 'ro-', label=splitted_col[2], ms=2)
-                    if 'miiv' in col: 
-                        ax.plot(range(0,51), data[col], 'mo-', label=splitted_col[2], ms=2)
-                    if 'hirid' in col: 
-                        ax.plot(range(0,51), data[col], 'co-', label=splitted_col[2], ms=2)
-        ax.legend()
+                for dataset_name, color in colors.items():
+                    if dataset_name in col:
+                        ax.plot(range(0, 51), data[col], color=color, marker='o', linestyle='-', ms=3, alpha=0.8)
         ax.grid()
-        ax.set_xlabel('Number of features')
-        ax.set_title(f'Trained on {group_combination}')
+        ax.tick_params(axis='x', labelsize=15, size=10) 
+        ax.tick_params(axis='y', labelsize=15, size=10)  
+        ax.set_xlabel('Number of features', fontsize=15)
+        ax.set_title(f'Trained on {group_combination}', fontsize=16)
         i += 1
-    fig.suptitle('Lasso Baseline')
-plt.tight_layout()
+    ax_legend = fig.add_subplot(1, 1, 1, frameon=False)
+    ax_legend.axis('off')
+    for dataset_name, color in colors.items():
+        ax_legend.plot([], [], color=color, label=dataset_name)
+    ax_legend.legend(title='', loc='center left', bbox_to_anchor=(1.05, 0.5), fontsize=16)
+    plt.tight_layout(rect=[0, 0, 0.95, 1])
+    if r==2:
+        plt.savefig(f'Images/{outcome}_two_train_lasso_baseline.png')
+    else:
+        plt.savefig(f'Images/{outcome}_three_train_lasso_baseline.png')
 
 # This block visualizes the DSL model's performance for each combination of 2 and 3 datasets, considering multiple alpha values.
 # It highlights how the performance varies with different regularization strengths by mapping alpha values to a color gradient.
@@ -71,7 +73,7 @@ for r in range(2, 4):
         data = pd.read_parquet(
             f'dsl_results/multiple alpha/{outcome}/{outcome}_multiple_alphas_train_on_{group_combination}_forward_selection_results.parquet')
         alphas = data['alpha'].unique()
-        colors = {'eicu': 'blue', 'mimic': 'red', 'miiv': 'magenta', 'hirid': 'cyan'}
+        colors = {'eicu': 'blue', 'hirid': 'cyan', 'miiv': 'magenta', 'mimic': 'red'}
         norm = plt.Normalize(vmin=min(alphas), vmax=max(alphas))
         cmap = plt.get_cmap('viridis')
         scalar_map = ScalarMappable(norm=norm, cmap=cmap)
@@ -85,26 +87,28 @@ for r in range(2, 4):
                         break
                 if dataset_name:
                     if 'name' not in col:
-                        # if 'train' in col:
-                        #     color = scalar_map.to_rgba(alpha)
-                        #     ax.plot(range(0, 51), data[data['alpha'] == alpha][col].iloc[0],
-                        #             color=color, alpha=1, marker='o', linestyle='-',
-                        #             ms=2, label='train')
                         if 'test' in col:
                             color = scalar_map.to_rgba(alpha)
                             ax.plot(range(0, 51), data[data['alpha'] == alpha][col].iloc[0],
                                     color=color, alpha=0.8, marker='o', linestyle='-',
                                     ms=2, label='test')
         ax.grid()
-        ax.set_xlabel('Number of features')
-        ax.set_title(f'Trained on {group_combination}')
+        ax.tick_params(axis='x', labelsize=15, size=10) 
+        ax.tick_params(axis='y', labelsize=15, size=10)  
+        ax.set_xlabel('Number of features', fontsize=15)
+        ax.set_title(f'Trained on {group_combination}', fontsize=16)
         i += 1
-    fig.suptitle('DSL')
     plt.tight_layout()
-    plt.colorbar(scalar_map, ax=axs, orientation='vertical', label='Alpha')
+    cbar = plt.colorbar(scalar_map, ax=axs, orientation='vertical', label='Regularization')
+    cbar.ax.yaxis.label.set_fontsize(14)
+    cbar.ax.tick_params(labelsize=12)  # Set font size for colorbar labels
+    if r==2:
+        plt.savefig(f'Images/{outcome}_color_map_two_train_dsl_multiple_alpha.png')
+    else:
+        plt.savefig(f'Images/{outcome}_color_map_three_train_dsl_multiple_alpha.png')
 
 # Adds a dedicated color legend for better dataset distinction in DSL model performance visualization.
-colors = {'eicu': 'blue', 'mimic': 'red', 'miiv': 'magenta', 'hirid': 'cyan'}
+colors = {'eicu': 'blue', 'hirid': 'cyan', 'miiv': 'magenta', 'mimic': 'red'}
 for r in range(2, 4):
     if r == 2:
         fig, axs = plt.subplots(2,3,figsize=(12,9))
@@ -133,16 +137,20 @@ for r in range(2, 4):
                             if dataset_name in col:
                                 ax.plot(range(0, 51), data[data['alpha']==alpha][col].iloc[0], color=color, marker='o', linestyle='-', ms=2, alpha=0.8)
         ax.grid()
-        ax.set_xlabel('Number of features')
-        ax.set_title(f'Trained on {group_combination}')
+        ax.tick_params(axis='x', labelsize=15, size=10) 
+        ax.tick_params(axis='y', labelsize=15, size=10)  
+        ax.set_xlabel('Number of features', fontsize=15)
+        ax.set_title(f'Trained on {group_combination}', fontsize=16)
         i += 1
     # Create a color legend subplot
     ax_legend = fig.add_subplot(1, 1, 1, frameon=False)
     ax_legend.axis('off')
     for dataset_name, color in colors.items():
         ax_legend.plot([], [], color=color, label=dataset_name)
-    ax_legend.legend(title='Color Legend', loc='center left', bbox_to_anchor=(1.05, 0.5))
-
-    fig.suptitle('DSL')
+    ax_legend.legend(title='', loc='center left', bbox_to_anchor=(1.05, 0.5), fontsize=16)
     plt.tight_layout(rect=[0, 0, 0.95, 1])
+    if r==2:
+        plt.savefig(f'Images/{outcome}_two_train_dsl_multiple_alpha.png')
+    else:
+        plt.savefig(f'Images/{outcome}_three_train_dsl_multiple_alpha.png')
 plt.show()
